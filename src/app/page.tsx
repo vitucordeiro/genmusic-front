46e0aa93dea3dede563c445ed7d21e3307b5129c
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 19:59:10 +0000

    Fix: delete imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import { Loader2 } from "lucide-react"
-import { Button } from "@/components/ui/button";
+
 import Nav from "@/components/screen/Nav";
 
 export default function Home() {
