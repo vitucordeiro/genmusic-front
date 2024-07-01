717a38b93933616a366a44571c03a5aed8c20828
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 20:41:45 +0000

    Feat: add Nav and Main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-
+import Main from "@/components/screen/Main";
 import Nav from "@/components/screen/Nav";
 
 export default function Home() {
   return (
-    <>  
-      <Nav></Nav>
-    </>
+    <div className="flex flex-col items-center justify-between w-screen" >   
+      <Nav/>
+      <Main/>
+    </div>
   );
 }
