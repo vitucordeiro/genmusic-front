dd27c7fe938de65b97ae05974927ac0e90593adc
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 21:25:14 +0000

    Refactor: CSS Layout Main

--- src/components/screen/Main.tsx
+++ src/components/screen/Main.tsx
@@ -1,12 +1,10 @@
+
 export default function Main(){
     return(
-        <div className="mt-28 border border-cyan-200 mt-10 flex flex-row justify-between w-2/3">
-            <div>
-                <h1 className="text-8xl font-extrabold  max-w-5xl">Create playlist based on your emotions with IA </h1>
-                <span className="mt-10 text-2xl">Your emotions deserve a soundtrack. Create and listen on your
+        <div className="mt-28 border border-cyan-200 mt-10 space-y-6 flex flex-col justify-between w-2/3">
+                <h1 className="text-8xl font-extrabold ">Create playlist based on your emotions with IA </h1>
+                <span className="text-2xl">Your emotions deserve a soundtrack. Create and listen on your
                 Spotify.</span>
-            </div>
-
         </div>
     )
 }
