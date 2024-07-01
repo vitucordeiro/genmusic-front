50779611ee49b8681e7140751a0cd4c82606ee76
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 21:23:54 +0000

    Refactor: Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Main from "@/components/screen/Main";
 import Nav from "@/components/screen/Nav";
 
 export default function Home() {
+
   return (
-    <div className="flex flex-col items-center justify-between w-screen" >   
+    <div className="flex flex-col items-center justify-between w-screen " >   
       <Nav/>
       <Main/>
     </div>
