0a03b7514ec0e0cdd5a7d94feef7a0d3aed9ff66
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 20:42:24 +0000

    Feat: create Nav

--- src/components/screen/Nav.tsx
+++ src/components/screen/Nav.tsx
@@ -1,12 +1,7 @@
-"use client"
-import { Button } from "../ui/button"
-import Link from 'next/link'
 import ButtonContext from "../atomics/ButtonContext"
-
 
 export default function Nav() {
     return(
-        <div className="flex  flex-col items-center justify-between w-screen h-screen ">
             <div className="border border-cyan-100 mt-10 flex items-center justify-between w-2/3">
                 <h3 className="font-extrabold text-lg">genMusic</h3>
                 <div className="p-4">
@@ -14,9 +9,5 @@ export default function Nav() {
                     <ButtonContext variant={"default"} link="singup" text="Get started"></ButtonContext>
                 </div>
             </div>
-
-
-
-
-        </div>)
+        )
 } ;
