70fe352e8257349fe4a18264dccee27377073aed
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-01 20:42:05 +0000

    Feat: create Main

---
+++ src/components/screen/Main.tsx
@@ -0,0 +1,12 @@
+export default function Main(){
+    return(
+        <div className="mt-28 border border-cyan-200 mt-10 flex flex-row justify-between w-2/3">
+            <div>
+                <h1 className="text-8xl font-extrabold  max-w-5xl">Create playlist based on your emotions with IA </h1>
+                <span className="mt-10 text-2xl">Your emotions deserve a soundtrack. Create and listen on your
+                Spotify.</span>
+            </div>
+
+        </div>
+    )
+}
