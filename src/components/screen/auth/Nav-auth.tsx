c898e1e5f93461e113d6a469d93c6746c05d60be
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 18:20:55 +0000

    feat: component to sign pages

---
+++ src/components/screen/auth/Nav-auth.tsx
@@ -0,0 +1,18 @@
+import { ChevronLeft } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+export default function Nav() {
+    return(
+            <div className="mt-10 flex flex-col items-center md:flex-row md:flex md:items-center md:justify-between justify-center w-2/3">
+                <h3 className="font-extrabold text-4xl">genMusic</h3>
+                <div className="flex flex-col items-center space-y-5 md:space-y-0 md:space-x-10 md:flex md:flex-row p-4 ">
+                <Link href={"/"}> 
+                    <Button size="icon">
+                        <ChevronLeft className="h-4 w-4"/> 
+                    </Button>
+                </Link>
+
+                </div>
+            </div>
+        )
+} ;
