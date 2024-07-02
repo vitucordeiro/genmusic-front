3369e1eee734aeec2f48443b1b90f69287283df9
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 16:07:07 +0000

    Fix: adjust layout footer

--- src/components/screen/home/Footer.tsx
+++ src/components/screen/home/Footer.tsx
@@ -2,12 +2,13 @@ import Link from "next/link"
 export default function Footer(){
     return(
         <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="https://flowbite.com/" className="hover:underline">Flowbite™</Link>. All Rights Reserved.
-            </span>
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between space-x-3">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href="#" className="hover:underline">genMusic™</Link>. All Rights Reserved.</span>
+
+
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <Link href="#" className="hover:underline me-4 md:me-6">About</Link>
+                    <Link href="#" className="hover:underline me-4 md:me-6"> About</Link>
                 </li>
                 <li>
                     <Link href="#" className="hover:underline me-4 md:me-6">Privacy Policy</Link>
