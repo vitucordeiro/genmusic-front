e55f791053caaf10cb3e2308b86af80326ab6a50
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 18:20:12 +0000

    Refactor: change name of components from screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/screen/home/Footer";
 import Main from "@/components/screen/home/Main";
-import Nav from "@/components/screen/home/Nav";
+import Nav from "@/components/screen/home/Nav-home";
 
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center justify-between w-screen h-screen " >   
+    <div className="flex flex-col items-center justify-between w-screen h-screen" >   
       <Nav/>
       <Main/>
       <Footer/>
