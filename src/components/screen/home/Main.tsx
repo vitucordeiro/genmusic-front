fefda0540cdaa51078ea315d205da68f7674af10
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 13:35:17 +0000

    Feat: add responsive main

--- src/components/screen/home/Main.tsx
+++ src/components/screen/home/Main.tsx
@@ -2,11 +2,11 @@ import ButtonContext from "../../atomics/ButtonContext";
 
 export default function Main(){
     return(
-        <div className="mt-28 border border-cyan-200  space-y-6 flex flex-col justify-between w-2/3">
-                <h1 className="text-8xl font-extrabold ">Create playlist based on your emotions with IA </h1>
-                <span className="text-2xl">Your emotions deserve a soundtrack. Create and listen on your
+        <div className="border border-cyan-200  space-y-1 md:space-y-5 lg:space-y-10 flex flex-col justify-between w-2/3">
+                <h1 className="text-2xl md:text-6xl lg:text-8xl font-extrabold ">Create playlist based on your emotions with IA </h1>
+                <span className="text-lg">Your emotions deserve a soundtrack. Create and listen on your
                 Spotify.</span>
-                <ButtonContext link="login" text="Create playlist" variant={"default"} size="w-1/4 h-12"></ButtonContext>
+                <ButtonContext link="login" text="Create playlist" variant={"default"} size="md:w-1/4 md:min-w-1/4 h-12 w-full"></ButtonContext>
         </div>
     )
 }
