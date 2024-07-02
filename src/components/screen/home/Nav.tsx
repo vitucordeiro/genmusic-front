de8c367c2aaeb23797f93dbc691250a68ba0c8a7
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 13:19:13 +0000

    Refactor: add responsive nav

--- src/components/screen/home/Nav.tsx
+++ src/components/screen/home/Nav.tsx
@@ -2,9 +2,9 @@ import ButtonContext from "../../atomics/ButtonContext"
 
 export default function Nav() {
     return(
-            <div className="border border-cyan-100 mt-10 flex items-center justify-between w-2/3">
+            <div className="border border-cyan-100 mt-10 flex flex-col items-center md:flex-row md:flex md:items-center md:justify-between justify-center w-2/3">
                 <h3 className="font-extrabold text-4xl">genMusic</h3>
-                <div className="p-4 space-x-8">
+                <div className="flex flex-col items-center space-y-5 md:space-x-5 md:flex md:flex-row p-4 ">
                     <ButtonContext variant={"ghost"} link="sigin" text="login" size="w-32"></ButtonContext>
                     <ButtonContext variant={"default"} link="singup" text="Get started" size="w-40"></ButtonContext>
                 </div>
