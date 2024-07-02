1d3d0818a36c713ac2461af583aee19a882a5710
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 13:22:28 +0000

    Fix: bug tailwind responsive nav

--- src/components/screen/home/Nav.tsx
+++ src/components/screen/home/Nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
     return(
             <div className="border border-cyan-100 mt-10 flex flex-col items-center md:flex-row md:flex md:items-center md:justify-between justify-center w-2/3">
                 <h3 className="font-extrabold text-4xl">genMusic</h3>
-                <div className="flex flex-col items-center space-y-5 md:space-x-5 md:flex md:flex-row p-4 ">
+                <div className="border border-cyan-700 flex flex-col items-center md:justify-center space-y-5 md:space-y-0 md:space-x-10 md:flex md:flex-row p-4 ">
                     <ButtonContext variant={"ghost"} link="sigin" text="login" size="w-32"></ButtonContext>
                     <ButtonContext variant={"default"} link="singup" text="Get started" size="w-40"></ButtonContext>
                 </div>
