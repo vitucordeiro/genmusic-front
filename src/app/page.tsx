09a3c8075fb657ea1be18e2b53763e0abae6e4c6
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-02 13:09:39 +0000

    Feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Main from "@/components/screen/Main";
-import Nav from "@/components/screen/Nav";
+import Footer from "@/components/screen/home/Footer";
+import Main from "@/components/screen/home/Main";
+import Nav from "@/components/screen/home/Nav";
 
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center justify-between w-screen " >   
+    <div className="flex flex-col items-center justify-between w-screen h-screen " >   
       <Nav/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
