42d435610ed6ac6d448eeb1ef54942fa252b10d9
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-03 00:50:17 +0000

    feat: add middleware with clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,30 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/forum(.*)',
+]);
+const isPublicRoute = createRouteMatcher([
+  '/(.*)',
+  '/auth/sign-in(.*)',
+  '/auth/sign-up(.*)'
+
+])
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)){
+    const token = auth().userId
+    if(!token){ return auth().redirectToSignIn()}
+    return console.log(token)
+
+  } 
+if(isPublicRoute(req)){
+  return console.log("opa")
+  
+}
+  
+
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
