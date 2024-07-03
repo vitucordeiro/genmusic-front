dda6d046234d2cf4a6e120cce77c8917ced12a93
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-03 17:57:48 +0000

    !fix: trying to fixing a bug in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from 'next/server';
+
+
 const isPrivateRoute = createRouteMatcher([
   '/dashboard(.*)',
-  '/forum(.*)',
 ]);
 const isPublicRoute = createRouteMatcher([
   '/(.*)',
@@ -12,19 +12,8 @@ const isPublicRoute = createRouteMatcher([
 
 ])
 export default clerkMiddleware((auth, req) => {
-  if(isPrivateRoute(req)){
-    if(!auth().sessionId && !auth().sessionId){
-      console.log("is not auth")
-      return auth().redirectToSignIn()
-    }
-    console.log("private router") 
-    return NextResponse.redirect('/')
-  }
-  if(isPublicRoute(req)){
-    console.log("public router")
-    return NextResponse.next();
-  }
-});
+  return console.log(auth().getToken())
+},{ debug:true});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
