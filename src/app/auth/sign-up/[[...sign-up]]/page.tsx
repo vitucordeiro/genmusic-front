c62b1768f922e904f288afb5154b0a1e723af814
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-03 03:45:33 +0000

    Fix: wrong name of component

--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,15 @@
 import Nav from "@/components/screen/auth/Nav-auth";
+import Sign from "@/components/screen/auth/Sign";
 import Footer from "@/components/screen/home/Footer";
 
-export default function SignUp(){
+export default function SignIn(){
     return(
+        <>
         <div className="flex flex-col items-center justify-between w-screen h-screen">
             <Nav/>
+            <Sign></Sign>
             <Footer/>
         </div>
+        </>
     )
 }
