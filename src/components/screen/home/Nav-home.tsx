579294cc0d5ecfcf24e87222b7a55226909f3a2c
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-03 03:46:15 +0000

    Fix: wrong declaration path's

--- src/components/screen/home/Nav-home.tsx
+++ src/components/screen/home/Nav-home.tsx
@@ -5,8 +5,8 @@ export default function Nav() {
             <div className="mt-10 flex flex-col items-center md:flex-row md:flex md:items-center md:justify-between justify-center w-2/3">
                 <h3 className="font-extrabold text-4xl">genMusic</h3>
                 <div className="flex flex-col items-center space-y-5 md:space-y-0 md:space-x-10 md:flex md:flex-row p-4 ">
-                    <ButtonContext variant={"ghost"} link="auth/signin" text="login" size="w-32"></ButtonContext>
-                    <ButtonContext variant={"default"} link="auth/signup" text="Get started" size="w-40"></ButtonContext>
+                    <ButtonContext variant={"ghost"} link="auth/sign-in" text="login" size="w-32"></ButtonContext>
+                    <ButtonContext variant={"default"} link="auth/sign-up" text="Get started" size="w-40"></ButtonContext>
                 </div>
             </div>
         )
