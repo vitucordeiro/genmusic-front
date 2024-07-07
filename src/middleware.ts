ff1ac3f3cb021b4b4747045c1b6783e6bac040c6
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-07 17:37:29 +0000

    fix: changed ClerkProvider location

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+const isOnboardingRoute = createRouteMatcher(["/dashboard"])
+const isPublicRoute = createRouteMatcher(["/public-route-example"])
 
-
-const isPrivateRoute = createRouteMatcher([
-  '/dashboard(.*)',
-]);
-const isPublicRoute = createRouteMatcher([
-  '/(.*)',
-  '/auth/sign-in(.*)',
-  '/auth/sign-up(.*)'
-
-])
-export default clerkMiddleware((auth, req) => {
-  return console.log(auth().getToken())
-},{ debug:true});
+export default clerkMiddleware((auth, req: NextRequest) => {
+  const {sessionClaims, sessionId} = auth()
+  const user = auth().userId;
+  console.log(sessionClaims, sessionId, user)
+  return NextResponse.next();
+   
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
