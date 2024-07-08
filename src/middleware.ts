987887e2e29086e135426bb5c71461a4f2a083ec
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-08 01:20:16 +0000

    refactor: removing unecessary variables

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
-const isOnboardingRoute = createRouteMatcher(["/dashboard"])
 const isPublicRoute = createRouteMatcher(["/", "/sign-in", "/sign-up"])
-import {AuthObject} from '@clerk/backend/internal'
-import { useAuth } from "@clerk/nextjs";
+
 export default clerkMiddleware((auth, req: NextRequest) => {
   const a = auth().actor;
-  //console.log("info:", a)
   const {sessionClaims, sessionId, userId} = auth()
-  console.log( sessionId)
   if(!isPublicRoute){
     if(!userId && !sessionId){
-      console.log("!use and !sessionId")
       auth().protect()
     }
-    console.log("com acesso ")
     return NextResponse.next();
   }
   return NextResponse.next();
