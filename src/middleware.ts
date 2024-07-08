d4f9e36e961e242136d0b19e11c0ac93a7659e2c
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-08 01:10:29 +0000

    fix: middleware fix bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 const isOnboardingRoute = createRouteMatcher(["/dashboard"])
-const isPublicRoute = createRouteMatcher(["/public-route-example"])
-
+const isPublicRoute = createRouteMatcher(["/", "/sign-in", "/sign-up"])
+import {AuthObject} from '@clerk/backend/internal'
+import { useAuth } from "@clerk/nextjs";
 export default clerkMiddleware((auth, req: NextRequest) => {
-  const {sessionClaims, sessionId} = auth()
-  const user = auth().userId;
-  console.log(sessionClaims, sessionId, user)
+  const a = auth().actor;
+  //console.log("info:", a)
+  const {sessionClaims, sessionId, userId} = auth()
+  console.log( sessionId)
+  if(!isPublicRoute){
+    if(!userId && !sessionId){
+      console.log("!use and !sessionId")
+      auth().protect()
+    }
+    console.log("com acesso ")
+    return NextResponse.next();
+  }
   return NextResponse.next();
+
    
 });
 
