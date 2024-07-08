44d06db80a53ba504756d3d056291958042b2722
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-08 01:11:03 +0000

    refactor: new dashboard

---
+++ src/app/dashboard/[[...rest]]/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { Protect, SignedIn, SignedOut, UserButton, UserProfile } from "@clerk/nextjs";
+import Link  from "next/link";
+
+export default function Home() {
+
+ 
+
+
+ return (
+    <div>
+
+        <SignedIn>
+        <Protect>
+            
+        <UserButton></UserButton>
+        <UserProfile></UserProfile>
+
+        </Protect>
+        </SignedIn>
+        <SignedOut>
+            <Link href="/sign-in">sign-in</Link>
+        </SignedOut>
+    </div>
+ )
+}
