ab6eee017bdb86879717e8da283f94b50ebaf738
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-09 16:49:34 +0000

    feat: add a cool conditional layout 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,74 @@
+import Nav from '@/components/screen/404/Nav-404'
+import Footer from '@/components/screen/home/Footer'
+import { Button } from '@/components/ui/button'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
+import { MoveLeft} from 'lucide-react'
 
 export default function NotFound() {
   return (
   <div>
+
     <SignedIn>
-        <div className='flex flex-'>
-            <h1>The page wasnt not found</h1>
+
+        <div className='flex flex-col justify-between items-center h-screen w-screen'>
+         
+          <Nav isLogged={true}/>
+          
+          <div className='flex flex-col items-center space-y-5'>
+           
+            <h1 className='font-black from-neutral-800 text-7xl'>Ooops!</h1>
+            
+            <span className='text-3xl from-neutral-600'>We couln&apos;t find the page you were looking for</span>
+            
+            <Link href={'/'}>
+              
+              <Button 
+                className='flex items-center px-4 py-2 rounded text-base font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 shadow-sm' 
+                variant={'link'}>
+              
+                <MoveLeft className="mr-2"/> go to home
+            
+              </Button>
+           
+            </Link>
+          
+          </div>
+         
+          <Footer/>
+        
         </div>
 
-
     </SignedIn>
+    
     <SignedOut>
-    <h1>The page wasnt not found</h1>
+      <div className='flex flex-col justify-between items-center h-screen w-screen'>
+
+        <Nav isLogged={false}/>
+
+        <div className='flex flex-col items-center space-y-5'>
+
+            <h1 className='font-black from-neutral-800 text-7xl'>Ooops!</h1>
+
+            <span className='text-3xl from-neutral-600'>We couln&apos;t find the page you were looking for</span>
+            
+            <Link href={'/'}>
+              
+              <Button 
+                className='flex items-center px-4 py-2 rounded text-base font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 shadow-sm' 
+                variant={'link'}>
+                
+                <MoveLeft className="mr-2"/> go to home
+              
+              </Button>
+            
+            </Link>
+          
+          </div>
+          
+          <Footer/>
+
+      </div>
 
     </SignedOut>
   </div>
--- src/components/screen/404/Nav-404.tsx
+++ src/components/screen/404/Nav-404.tsx
@@ -1,8 +1,23 @@
-export default function Nav(){
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function Nav({isLogged}:{isLogged:boolean}) {
     return(
-        <div className="flex flex-row w-screen items-center">
-
-
-        </div>
-    )
-}
+            <div className="mt-10 flex flex-col items-center md:flex-row md:flex md:items-center md:justify-between justify-center w-2/3">
+                <h3 className="font-extrabold text-4xl">genMusic</h3>
+                <div className="flex flex-col items-center space-y-5 md:space-y-0 md:space-x-10 md:flex md:flex-row p-4 ">
+                    {isLogged === true ? ( 
+                        <UserButton />
+                    ) : (
+                        <Link href="/">
+                            <Button size="icon">
+                            <ChevronLeft className="h-4 w-4" />
+                            </Button>
+                        </Link>
+                        )}
+                </div>
+            </div>
+        )
+} ;
