a3a4a548a2fce2e0eb88ff09ea0b4f4b5b7c2f56
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-09-09 20:23:49 -0300

    refactor: blank spaces deleted

--- src/app/dashboard/[[...rest]]/layout.tsx
+++ src/app/dashboard/[[...rest]]/layout.tsx
@@ -16,34 +16,24 @@ export default async function RootLayout({
    <div>
 
     <SignedIn>
-    <Protect>
-    {children}
+      <Protect>
+      {children}
   
     </Protect>
     </SignedIn>
     <SignedOut>
     <div className='flex flex-col justify-between items-center h-screen w-screen'>
-
       <Nav isLogged={false}/>
-
       <div className='flex flex-col items-center space-y-5'>
-
           <h1 className='font-black from-neutral-800 text-7xl'>Ooops!</h1>
-
           <span className='text-3xl from-neutral-600'>We couln&apos;t find the page you were looking for</span>
-          
           <Link href={'/'}>
-            
             <Button 
               className='flex items-center px-4 py-2 rounded text-base font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 shadow-sm' 
               variant={'link'}>
-              
               <MoveLeft className="mr-2"/> go to home
-            
             </Button>
-          
           </Link>
-        
         </div>
         
         <Footer/>
